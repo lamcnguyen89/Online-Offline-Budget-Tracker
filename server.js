1dd0cadee9e0ac69f4749b8218c2f21419bd9afd
Lam Nguyen <lamcnguyen89@gmail.com>
2020-08-06 20:55:49 -0400

    Added code to deploy to Heroku

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+var MONGODB_URI = (process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
