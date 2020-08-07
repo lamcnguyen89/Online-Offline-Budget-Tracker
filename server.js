f3a3c713cc0fc8d667e23b7e91092de442995e1a
Lam Nguyen <lamcnguyen89@gmail.com>
2020-08-06 21:17:57 -0400

    Corrected server.js to deploy to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://malneyungfl:nguyen05041989@mongodb://<dbuser>:<dbpassword>@ds331558.mlab.com:31558/heroku_bks1xvv2"
 
 const PORT = 3000;
 
@@ -15,12 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = (process.env.MONGODB_URI || "mongodb://localhost/budget", {
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
-});
-
-mongoose.connect(MONGODB_URI);
+} );
 
 // routes
 app.use(require("./routes/api.js"));
